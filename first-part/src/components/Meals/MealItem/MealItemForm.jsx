014b616a1e92e2aafaa8ca52520fc9f22c51f2d4
SergEnergy <kis8883244@gmail.com>
2023-10-09 09:46:51 +0300

    feat: add custom Input component

--- first-part/src/components/Meals/MealItem/MealItemForm.jsx
+++ first-part/src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,7 +1,25 @@
 import classes from './MealItemForm.module.css';
+import Input from '../../UI/Input/Input';
 
-function MealItemForm() {
-	return (  );
+function MealItemForm(props) {
+  const id = new Date().getMilliseconds().toString();
+
+  return (
+    <form className={classes.form}>
+      <Input
+        label={'Amount'}
+        input={{
+          id: id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button className=''>+ Add</button>
+    </form>
+  );
 }
 
 export default MealItemForm;
