93de5c66dae1b7b1012465544fd335eb16c96a89
SergEnergy <kis8883244@gmail.com>
2023-10-10 10:14:17 +0300

    fix: fix bugs with context modal

--- first-part/src/App.jsx
+++ first-part/src/App.jsx
@@ -1,33 +1,21 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 
 import ModalContext from './contexts/ModalContext';
-import { ModalContextProvider } from './contexts/ModalContext';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
   const context = useContext(ModalContext);
-  const [isShowed, setIsShowed] = useState(false);
-
-  useEffect(() => {
-    if (context.isVisible) {
-      console.log('visible-->', context.isVisible);
-      setIsShowed(true);
-    } else {
-      console.log('invisible-->', context.isVisible);
-      setIsShowed(false);
-    }
-  }, [context.isVisible]);
 
   return (
-    <ModalContextProvider>
-      {isShowed && <Cart />}
+    <>
+      {context.isVisible && <Cart />}
       <Header />
       <main className=''>
         <Meals />
       </main>
-    </ModalContextProvider>
+    </>
   );
 }
 
--- first-part/src/index.jsx
+++ first-part/src/index.jsx
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom/client';
+import { ModalContextProvider } from './contexts/ModalContext';
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ModalContextProvider>
+    <App />
+  </ModalContextProvider>
+);
