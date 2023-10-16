53988ce7d74f359e7f812df67752639201b7e3f6
SergEnergy <kis8883244@gmail.com>
2023-10-16 22:03:59 +0300

    feat: add animation in cart button with effect and state

--- first-part/src/components/Layout/HeaderCartButton.jsx
+++ first-part/src/components/Layout/HeaderCartButton.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ModalContext from '../../contexts/ModalContext';
 import CartContext from '../../contexts/CartContext';
 
@@ -6,16 +6,31 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 function HeaderCartButton(props) {
+  const [btnIsAnimated, setBtnIsAnimated] = useState(false);
   const context = useContext(ModalContext);
   const cartContext = useContext(CartContext);
+  const { items } = cartContext;
 
-  const amountOfCartItem = cartContext.items.reduce((acc, curr) => {
+  const amountOfCartItem = items.reduce((acc, curr) => {
     return curr.amount + acc;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsAnimated ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setBtnIsAnimated(true);
+    const timerId = setTimeout(() => {
+      setBtnIsAnimated(false);
+    }, 300);
+    return () => {
+      clearTimeout(timerId);
+    }; //300 - is a duration of animation in css
+  }, [items]);
+
   return (
     <>
-      <button className={classes.button} onClick={context.showHandler}>
+      <button className={btnClasses} onClick={context.showHandler}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
