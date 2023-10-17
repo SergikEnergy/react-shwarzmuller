b29708ae515a1a38036237afb5b0003f30d40bfd
SergEnergy <kis8883244@gmail.com>
2023-10-17 13:59:31 +0300

    add: example for useMemo using

--- first-part/src/App.jsx
+++ first-part/src/App.jsx
@@ -1,24 +1,14 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 
 import './App.css';
 import { MemoButton } from './components/UI/Button/Button';
-import DemoOutput from './components/Demo/DemoOutput';
+import DemoList from './components/Demo/DemoList';
 
 function App() {
-  const [show, setShow] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTile] = useState('My List');
 
-  const toggleHandler = useCallback(() => {
-    if (allowToggle) {
-      setShow((prev) => !prev);
-    }
-  }, [allowToggle]);
-  //useCallback allows to store function during components execution
-  //empty array of dependencies guarantee react that inside callback function won't be change
-  // if we have some variables inside useCallback - react store them in your external store and never change it - this is the reason of using dependency array that show react when change this variables in your store and reassign the function
-
-  const allowToggling = () => {
-    setAllowToggle(true);
+  const changeTitleHandler = () => {
+    setListTile('New Title');
   };
 
   console.log('App RUNNING');
@@ -26,10 +16,8 @@ function App() {
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOutput show={show} />
-      <MemoButton onClick={allowToggling}>Allow Toggle</MemoButton>
-      <MemoButton onClick={toggleHandler}>Show Paragraph!</MemoButton>
-      {/*memo button was running again because props get an object with function toggleHandler and function is every time a new object and {}!=={} in JS every time because it is no primitive value in JS*/}
+      <DemoList title={listTitle} items={[5, 3, 1, 10, 9]} />
+      <MemoButton onClick={changeTitleHandler}>Change title</MemoButton>
     </div>
   );
 }
