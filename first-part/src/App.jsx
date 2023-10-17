e792c36752a8480fc32183bca81c299b76986b7a
SergEnergy <kis8883244@gmail.com>
2023-10-17 13:29:14 +0300

    feat: show that parametr inside useCalback without dependency never change

--- first-part/src/App.jsx
+++ first-part/src/App.jsx
@@ -6,19 +6,28 @@ import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [show, setShow] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   const toggleHandler = useCallback(() => {
-    setShow((prev) => !prev);
+    if (allowToggle) {
+      setShow((prev) => !prev);
+    }
   }, []);
   //useCallback allows to store function during components execution
   //empty array of dependencies guarantee react that inside callback function won't be change
+  // if we have some variables inside useCallback - react store them in your external store and never change it - this is the reason of using dependency array that show react when change this variables in your store and reassign the function
+
+  const allowToggling = () => {
+    setAllowToggle(true);
+  };
 
   console.log('App RUNNING');
 
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={show} />
+      <MemoButton onClick={allowToggling}>Allow Toggle</MemoButton>
       <MemoButton onClick={toggleHandler}>Show Paragraph!</MemoButton>
       {/*memo button was running again because props get an object with function toggleHandler and function is every time a new object and {}!=={} in JS every time because it is no primitive value in JS*/}
     </div>
