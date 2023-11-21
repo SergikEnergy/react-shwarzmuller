c7a15af42d41f0d8cd39fd29d245c4d2bd351b91
SergEnergy <kis8883244@gmail.com>
2023-11-21 16:35:25 +0300

    add: add base structure for module

--- first-part/src/App.jsx
+++ first-part/src/App.jsx
@@ -1,8 +1,51 @@
+import { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://dummyjson.com/todos');
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json()['todos'];
+
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey.id, text: taskKey.todo });
+      }
+
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks} />
+    </>
   );
 }
 
