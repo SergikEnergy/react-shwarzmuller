0a7d39f159ed25e8f25566322ba810b80ef4814d
SergEnergy <kis8883244@gmail.com>
2023-11-28 01:54:08 +0300

    feat: add handlers with ref and state

--- first-part/src/components/SimpleInput.jsx
+++ first-part/src/components/SimpleInput.jsx
@@ -1,9 +1,30 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [inputName, setInputName] = useState('');
+
+  const inputNameRef = useRef();
+
+  const inputNameChangeHandler = (event) => {
+    setInputName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(inputName);
+
+    console.log(inputNameRef.current.value);
+    // inputNameRef.current.value=''; this is not ideal - DON'T MANIPULATE THE DOM DIRECTLY
+    // using setState - more pleasant version
+    // input should be controlled - with useState and value or uncontrolled - with useRef and current.value - inly one approach
+    setInputName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={inputNameRef} value={inputName} onChange={inputNameChangeHandler} />
       </div>
       <div className='form-actions'>
         <button>Submit</button>
