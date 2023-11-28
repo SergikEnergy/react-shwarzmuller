23be6823d0dc9d43faf5b26c8624105064bb2e03
SergEnergy <kis8883244@gmail.com>
2023-11-28 05:53:02 +0300

    feat: add email with custom hook

--- first-part/src/components/SimpleInput.jsx
+++ first-part/src/components/SimpleInput.jsx
@@ -10,9 +10,18 @@ const SimpleInput = (props) => {
     reset: resetName,
   } = useInput((value) => value.trim().length !== 0);
 
+  const {
+    value: enteredEmail,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput((value) => value.includes('@'));
+
   let formIsValid = false;
 
-  if (nameIsValid) {
+  if (nameIsValid && emailIsValid) {
     formIsValid = true;
   }
 
@@ -23,9 +32,11 @@ const SimpleInput = (props) => {
       return;
     }
     resetName();
+    resetEmail();
   };
 
   const nameInputClasses = nameHasError ? 'form-control invalid' : 'form-control';
+  const emailInputClasses = emailHasError ? 'form-control invalid' : 'form-control';
 
   return (
     <form onSubmit={formSubmitHandler}>
@@ -33,6 +44,11 @@ const SimpleInput = (props) => {
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' value={enteredName} onChange={nameChangeHandler} onBlur={nameBlurHandler} />
         {nameHasError && <p className='error-text'>Name must not be empty.</p>}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor='email'>Your E-mail</label>
+        <input type='text' id='email' value={enteredEmail} onChange={emailChangeHandler} onBlur={emailBlurHandler} />
+        {emailHasError && <p className='error-text'>Please, enter a valid email.</p>}
       </div>
       <div className='form-actions'>
         <button disabled={!formIsValid}>Submit</button>
