d2a9034b073bfcf6b205c1c4e900132c030583d9
SergEnergy <kis8883244@gmail.com>
2023-11-28 06:29:52 +0300

    add: add another component with 3 fields form using custom hook

--- first-part/src/components/BasicForm.jsx
+++ first-part/src/components/BasicForm.jsx
@@ -1,22 +1,70 @@
+import useInput from '../hooks/useInput';
+
 const BasicForm = (props) => {
+  const {
+    value: nameInput,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    inputBlurHandler: nameBlur,
+    valueChangeHandler: nameChange,
+    reset: nameReset,
+  } = useInput((value) => value.trim().length !== 0);
+
+  const {
+    value: lastNameInput,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    inputBlurHandler: lastNameBlur,
+    valueChangeHandler: lastNameChange,
+    reset: lastNameReset,
+  } = useInput((value) => value.trim().length !== 0);
+
+  const {
+    value: email,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    inputBlurHandler: emailBlur,
+    valueChangeHandler: emailChange,
+    reset: emailReset,
+  } = useInput((value) => value.includes('@'));
+
+  let formIsValid = false;
+  if (nameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+    nameReset();
+    lastNameReset();
+    emailReset();
+  };
+
   return (
-    <form>
+    <form onSubmit={submitFormHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={`form-control ${nameHasError && 'invalid'}`}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' onChange={nameChange} onBlur={nameBlur} value={nameInput} />
+          {nameHasError && <p className='error-text'>Name should not be empty.</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={`form-control ${lastNameHasError && 'invalid'}`}>
+          <label htmlFor='lastName'>Last Name</label>
+          <input type='text' id='lastName' onChange={lastNameChange} onBlur={lastNameBlur} value={lastNameInput} />
+          {lastNameHasError && <p className='error-text'>Last name should not be empty.</p>}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={`form-control ${emailHasError && 'invalid'}`}>
+        <label htmlFor='email'>E-Mail Address</label>
+        <input type='text' id='email' onChange={emailChange} onBlur={emailBlur} value={email} />
+        {emailHasError && <p className='error-text'>Please, enter correct email.</p>}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
