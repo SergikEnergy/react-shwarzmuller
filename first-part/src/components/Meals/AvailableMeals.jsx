792084a050b8b1706aab67e0a5e431ec5c1b41fa
SergEnergy <kis8883244@gmail.com>
2023-11-29 20:52:32 +0300

    feat: add logic to error and loading state

--- first-part/src/components/Meals/AvailableMeals.jsx
+++ first-part/src/components/Meals/AvailableMeals.jsx
@@ -7,13 +7,20 @@ import classes from './AvailableMeals.module.css';
 
 function AvailableMeals() {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       try {
+        setIsError(null);
+        setIsLoading(true);
         const responseMeals = await fetch(
           'https://react-shwarzmuller-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
         );
+        if (!responseMeals.ok) {
+          throw new Error('Failed request to the database. Try repeat it later!');
+        }
         const dataMeals = await responseMeals.json();
         const arrFromDataMeals = [];
         for (const key in dataMeals) {
@@ -22,10 +29,11 @@ function AvailableMeals() {
             ...dataMeals[key],
           });
         }
+        setIsLoading(false);
         setMeals(arrFromDataMeals);
-        // console.log(arrFromDataMeals);
       } catch (err) {
-        console.log(err);
+        setIsLoading(false);
+        setIsError(err.message);
       }
     };
 
@@ -33,17 +41,37 @@ function AvailableMeals() {
   }, []);
 
   return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>
-          {meals.map((meal) => {
-            return (
-              <MealItem key={meal.id} id={meal.id} name={meal.name} price={meal.price} description={meal.description} />
-            );
-          })}
-        </ul>
-      </Card>
-    </section>
+    <>
+      {isLoading && (
+        <section className={classes['meals__loading']}>
+          <p>Loading...</p>
+        </section>
+      )}
+      {!isLoading && !isError && (
+        <section className={classes.meals}>
+          <Card>
+            <ul>
+              {meals.map((meal) => {
+                return (
+                  <MealItem
+                    key={meal.id}
+                    id={meal.id}
+                    name={meal.name}
+                    price={meal.price}
+                    description={meal.description}
+                  />
+                );
+              })}
+            </ul>
+          </Card>
+        </section>
+      )}
+      {isError && (
+        <section className={classes['meals__error']}>
+          <p>Something went wrong! Please, try to reload the app later.</p>
+        </section>
+      )}
+    </>
   );
 }
 
