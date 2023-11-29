0b581b017f4d409da6c82111e7fa7bdfb648874b
SergEnergy <kis8883244@gmail.com>
2023-11-29 20:26:38 +0300

    feat: add request for data to firebase realtime database

--- first-part/src/components/Meals/AvailableMeals.jsx
+++ first-part/src/components/Meals/AvailableMeals.jsx
@@ -1,15 +1,42 @@
+import { useEffect, useState } from 'react';
+
 import Card from '../UI/Card/Card.jsx';
 import MealItem from './MealItem/MealItem.jsx';
 
-import mealsData from '../../data/dummyMeals.js';
 import classes from './AvailableMeals.module.css';
 
 function AvailableMeals() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const responseMeals = await fetch(
+          'https://react-shwarzmuller-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+        );
+        const dataMeals = await responseMeals.json();
+        const arrFromDataMeals = [];
+        for (const key in dataMeals) {
+          arrFromDataMeals.push({
+            id: key,
+            ...dataMeals[key],
+          });
+        }
+        setMeals(arrFromDataMeals);
+        // console.log(arrFromDataMeals);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchMeals();
+  }, []);
+
   return (
     <section className={classes.meals}>
       <Card>
         <ul>
-          {mealsData.map((meal) => {
+          {meals.map((meal) => {
             return (
               <MealItem key={meal.id} id={meal.id} name={meal.name} price={meal.price} description={meal.description} />
             );
