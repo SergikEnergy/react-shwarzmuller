e085dbfa75cb0310e33fadd0184f82952835e855
SergEnergy <kis8883244@gmail.com>
2023-12-03 22:31:54 +0300

    feat: add logic for increment and decrement counter

--- first-part/src/components/Counter.jsx
+++ first-part/src/components/Counter.jsx
@@ -1,12 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 function Counter() {
+  const counter = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
   const toggleCounterHandler = () => {};
+
+  const incrementCounterHandler = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const decrementCounterHandler = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button className='' onClick={incrementCounterHandler}>
+          Increment
+        </button>
+        <button className='' onClick={decrementCounterHandler}>
+          Decrement
+        </button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
