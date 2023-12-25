2999264c7a1cfbd71e21beaa4054663b11b646f6
SergEnergy <kis8883244@gmail.com>
2023-12-25 18:34:22 +0300

    fix: avoid first initial request

--- first-part/src/App.jsx
+++ first-part/src/App.jsx
@@ -5,17 +5,28 @@ import Layout from './components/Layout/Layout';
 import Cart from './components/Cart/Cart';
 import Products from './components/Shop/Products';
 
+let isInitial = true;
+
 function App() {
   const isVisibleCart = useSelector((state) => state.ui.isCartVisible);
   const cart = useSelector((state) => state.cart);
+
   useEffect(() => {
-    fetch('https://react-shwarzmuller-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(cart),
-    });
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    const sendData = () => {
+      fetch('https://react-shwarzmuller-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(cart),
+      });
+    };
+    sendData();
   }, [cart]);
 
   return (
