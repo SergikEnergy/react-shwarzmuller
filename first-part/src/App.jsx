bd9a94ae4e82759d9b791f67d45be69088071cd8
SergEnergy <kis8883244@gmail.com>
2023-12-27 14:17:34 +0300

    another way to create route from elements

--- first-part/src/App.jsx
+++ first-part/src/App.jsx
@@ -1,14 +1,23 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
+
+const routesElements = createRoutesFromElements(
+  <Route>
+    <Route path='/' element={<Home />} />
+    <Route path='/products' element={<Products />} />
+  </Route>
+);
 
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
   { path: '/products', element: <Products /> },
 ]);
 
+const router2 = createBrowserRouter(routesElements);
+
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router2} />;
 }
 
 export default App;
