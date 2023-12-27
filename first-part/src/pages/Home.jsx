e93680396e85edc68b65367df498e1e6aefeea36
SergEnergy <kis8883244@gmail.com>
2023-12-27 14:26:45 +0300

    feat: use link for navigate

--- first-part/src/pages/Home.jsx
+++ first-part/src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
-  return <h1 className=''>My Homepage</h1>;
+  return (
+    <>
+      <h1 className=''>My Homepage</h1>
+      <p className=''>
+        Go to the
+        <a href='/products' className=''>
+          {/* if we use simple a link - we will get reload page - it's not, what we need */}
+          products list
+        </a>
+        Go to the
+        <Link to='/products'>the list of product</Link>.
+      </p>
+    </>
+  );
 }
 
 export default Home;
