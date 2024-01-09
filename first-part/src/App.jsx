dc9fe870a06c810a6b0553a6866d0f5f9611099d
SergEnergy <kis8883244@gmail.com>
2024-01-10 00:25:00 +0300

    add: add auth to route

--- first-part/src/App.jsx
+++ first-part/src/App.jsx
@@ -8,6 +8,7 @@ import EventsRootLayout from './pages/EventsRoot';
 import HomePage from './pages/Home';
 import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
+import AuthenticationPage from './pages/Authentication';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
@@ -51,6 +52,7 @@ const router = createBrowserRouter([
           },
         ],
       },
+      { path: 'auth', element: <AuthenticationPage /> },
       {
         path: 'newsletter',
         element: <NewsletterPage />,
